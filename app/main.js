dea3cd36fe0b2c0cbc1ffa111edac2b60f6c9338
Duc-Truong <ducqt00@gmail.com>
2024-06-30 10:35:20 +0700

    Respond with body

--- app/main.js
+++ app/main.js
@@ -3,12 +3,17 @@ const net = require("net");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-    if (request.startsWith("GET / ")) {
-      const httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
-      socket.write(httpResponse);
-      return;
-    }
+    const url = request.split(" ")[1];
     const httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+    if (url == "/") {
+      httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+    }
+    if (url.includes("/echo/")) {
+      const requestString = url.split("/echo/")[1];
+      httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n${requestString}`;
+    }
+
     socket.write(httpResponse);
   });
   socket.on("close", () => {
