9abe9b247549679f8d9d0039ebfc22a7f3c148c4
Duc-Truong <ducqt00@gmail.com>
2024-06-30 10:30:33 +0700

    Extract URL path

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ const net = require("net");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-    if (request.startsWith("GET /")) {
+    if (request.startsWith("GET / ")) {
       const httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
       socket.write(httpResponse);
       return;
