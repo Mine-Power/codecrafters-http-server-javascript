54d466454ac29d46f4337614c65bf43bd4dc1a8e
Duc-Truong <ducqt00@gmail.com>
2024-06-30 16:26:00 +0700

    Test compression headers

--- app/main.js
+++ app/main.js
@@ -17,7 +17,14 @@ const server = net.createServer((socket) => {
 
     if (path.includes("/echo/")) {
       const requestString = path.split("/echo/")[1];
-      httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${requestString.length}\r\n\r\n${requestString}`;
+      let encodingString = "";
+      if (headers[3].length != 0) {
+        const encoding = headers[3].split("Content-Encoding: ")[1];
+        if (encoding === "gzip") {
+          encodingString = `\r\nContent-Encoding: ${encoding}`
+        }
+      }
+      httpResponse = `HTTP/1.1 200 OK${encodingString}\r\nContent-Type: text/plain\r\nContent-Length: ${requestString.length}\r\n\r\n${requestString}`;
     }
 
     if (path.includes("/user-agent")) {
