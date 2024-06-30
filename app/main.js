fd6f3b24f1e9f7bf43804a02505a88fda5350567
Duc-Truong <ducqt00@gmail.com>
2024-06-30 11:26:25 +0700

    Read header

--- app/main.js
+++ app/main.js
@@ -3,7 +3,8 @@ const net = require("net");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-    const url = request.split(" ")[1];
+    const requestParts = request.split(" ");
+    const url = requestParts[1];
     let httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
 
     if (url == "/") {
@@ -12,6 +13,10 @@ const server = net.createServer((socket) => {
     if (url.includes("/echo/")) {
       const requestString = url.split("/echo/")[1];
       httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${requestString.length}\r\n\r\n${requestString}`;
+    }
+    if (url.includes("/user-agent")) {
+      const userAgent = requestParts[5];
+      httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
     }
 
     socket.write(httpResponse);
