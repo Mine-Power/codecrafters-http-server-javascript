bba24705c395bc0ca440dca155e49069183286bc
Duc-Truong <ducqt00@gmail.com>
2024-06-30 23:06:13 +0700

    Test refractoring

--- app/main.js
+++ app/main.js
@@ -2,70 +2,90 @@ const net = require("net");
 const fs = require("fs");
 const zlib = require("zlib");
 
+const RES_START = "HTTP/1.1";
+
 const readHeaders = (data) => {
-  const response = data.toString().split("\r\n");
-  const method = response[0].split(" ")[0];
-  const path = response[0].split(" ")[1];
-  const body = response[response.length - 1];
-  return [
-    method,
-    path,
-    Object.fromEntries(response.slice(1, -2).map((header) => [header.split(": ")[0], header.split(": ")[1]])),
-    body
-  ];
+  const req = data.toString().split("\r\n");
+  const method = req[0].split(" ")[0];
+  const path = req[0].split(" ")[1];
+  const headers = Object.fromEntries(req.slice(1, -2).map((header) => [header.split(": ")[0], header.split(": ")[1]]));
+  const body = req[req.length - 1];
+  return [method, path, headers, body];
+};
+
+const writeMessage = (socket, resStatus, resHeaders = [], body = "") => {
+  const resHeaderStrs = headers.map((header) => `${header[0]}: ${header[1]}`);
+  const res = [`${RES_START} ${resStatus}`, ...resHeaderStrs];
+  socket.write(res.join("\r\n"));
+  socket.write(body);
+}
+
+const handleData = (socket, data) => {
+  const [method, path, headers, body] = readHeaders(data);
+  // Debug
+  console.log(method, path, headers, body);
+
+  if (path == "/") {
+    writeMessage(socket, "200 OK");
+    return;
+  }
+
+  if (path.includes("/echo/")) {
+    let content = path.split("/echo/")[1];
+    const resHeaders = [
+      ["Content-Type", "text/plain"]
+    ];
+    if (headers["Accept-Encoding"] != null) {
+      const encodings = headers["Accept-Encoding"].split(", ");
+      encodings.forEach((encoding) => {
+        if (encoding === "gzip") {
+          resHeaders.push(["Content-Encoding", encoding]);
+          content = zlib.gzipSync(content);
+        }
+      })
+    }
+    resHeaders.push(["Content-Length", content.length]);
+    writeMessage(socket, "200 OK", resHeaders, content);
+    return;
+  }
+
+  if (path.includes("/user-agent")) {
+    const userAgent = headers["User-Agent"];
+    writeMessage(socket, "200 OK", [
+      "Content-Type", "text/plain",
+      "Content-Length", userAgent.length,
+    ], userAgent);
+    return;
+  }
+
+  if (path.includes("/files/")) {
+    const fileName = path.split("/files/")[1];
+    const directory = process.argv[3];
+    switch (method) {
+      case "GET":
+        if (fs.existsSync(`${directory}/${fileName}`)) {
+          const content = fs.readFileSync(`${directory}/${fileName}`).toString();
+          writeMessage(socket, "200 OK", [
+            "Content-Type", "application/octet-stream",
+            "Content-Length", content.length
+          ], content)
+        }
+        break;
+      case "POST":
+        fs.writeFileSync(`${directory}/${fileName}`, body);
+        writeMessage(socket, "201 Created");
+        break;
+      default:
+    }
+    return;
+  }
+
+  writeMessage(socket, "404 NOT FOUND");
 };
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const [method, path, headers, body] = readHeaders(data);
-    let httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
-    // Debug
-    console.log(method, path, headers, body);
-
-    if (path == "/") {
-      httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
-    }
-
-    if (path.includes("/echo/")) {
-      let content = path.split("/echo/")[1];
-      let encodingString = "";
-      if (headers["Accept-Encoding"] != null) {
-        const encodings = headers["Accept-Encoding"].split(", ");
-        encodings.forEach((encoding) => {
-          if (encoding === "gzip") {
-            encodingString = `\r\nContent-Encoding: ${encoding}`;
-            content = zlib.gzipSync(content);
-          }
-        })
-      }
-      httpResponse = `HTTP/1.1 200 OK${encodingString}\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
-    }
-
-    if (path.includes("/user-agent")) {
-      const userAgent = headers["User-Agent"];
-      httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
-    }
-
-    if (path.includes("/files/")) {
-      const fileName = path.split("/files/")[1];
-      const directory = process.argv[3];
-      switch (method) {
-        case "GET":
-          if (fs.existsSync(`${directory}/${fileName}`)) {
-            const content = fs.readFileSync(`${directory}/${fileName}`).toString();
-            httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`
-          }
-          break;
-        case "POST":
-          fs.writeFileSync(`${directory}/${fileName}`, body);
-          httpResponse = "HTTP/1.1 201 Created\r\n\r\n";
-          break;
-        default:
-          return
-      }
-    }
-
-    socket.write(httpResponse);
+    handleData(socket, data);
   });
   socket.on("close", () => {
     socket.end();
