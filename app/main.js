5c6c480722ca7ee1a86d27fec41e08d6829449d1
Duc-Truong <ducqt00@gmail.com>
2024-06-30 11:58:15 +0700

    Test

--- app/main.js
+++ app/main.js
@@ -5,26 +5,41 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
     const url = request.split(" ")[1];
+    const method = request.split(" ")[0];
     const headers = request.split("\r\n");
     let httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
 
     if (url == "/") {
       httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
     }
+
     if (url.includes("/echo/")) {
       const requestString = url.split("/echo/")[1];
       httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${requestString.length}\r\n\r\n${requestString}`;
     }
+
     if (url.includes("/user-agent")) {
       const userAgent = headers[2].split('User-Agent: ')[1];
       httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
     }
+
     if (url.includes("/files/")) {
       const fileName = url.split("/files/")[1];
       const directory = process.argv[3];
-      if (fs.existsSync(`${directory}/${fileName}`)) {
-        const content = fs.readFileSync(`${directory}/${fileName}`).toString();
-        httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+      switch (method) {
+        case "GET":
+          if (fs.existsSync(`${directory}/${fileName}`)) {
+            const content = fs.readFileSync(`${directory}/${fileName}`).toString();
+            httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+          }
+          break;
+        case "POST":
+          const content = headers[headers.length - 1];
+          fs.writeFileSync(`${directory}/${fileName}`, content);
+          httpResponse = "HTTP/1.1 201 Created\r\n\r\n";
+          break;
+        default:
+          return
       }
     }
 
